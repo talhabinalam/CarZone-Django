37821aa84aae159352836265caf23bb641e4ca1b
talhabinalam <talhabinalam98@gmail.com>
2023-08-27 23:44:36 +0600

    added team model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
+from typing import Any
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    designation = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='photos/team_profile')
+    facebook_link = models.URLField(max_length=150)
+    twitter_link = models.URLField(max_length=150)
+    google_plus_link = models.URLField(max_length=150)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.first_name
